0cf476e2b2194a3d312b860b1045e179848f81fe
Jerry <mailakshitagarwal@gmail.com>
2026-02-06 23:32:49 +0530

    Sync : 2026-02-06 23:32:49

--- FlaskDemo/app.py
+++ FlaskDemo/app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template,request
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Welcome to the Flask Demo on home page!'
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
