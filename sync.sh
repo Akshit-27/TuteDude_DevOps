7afff75cd4dbd769d0d0a3686398b913813db9a5
Jerry <mailakshitagarwal@gmail.com>
2026-02-06 22:36:20 +0530

    Sync : 2026-02-06 22:36:20

--- sync.sh
+++ sync.sh
@@ -4,10 +4,6 @@ set -e
 echo -e "Pushing codebase to github.."
 git status
 git add .
+git commit -m "Sync : $(date '+%Y-%m-%d %H:%M:%S')"
+git push origin main
 
-if git diff --cached --quiet; then
-	echo "No New Changes to commit"
-else
-	git commit -m "Sync : $(date '+%Y-%m-%d %H:%M:%S')"
-	git push origin main
-fi
