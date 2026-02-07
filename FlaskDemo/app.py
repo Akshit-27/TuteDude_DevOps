8864ae09260b35b4d7e3298a9d6836f9f81a7c4e
Jerry <mailakshitagarwal@gmail.com>
2026-02-08 00:18:07 +0530

    mongo code commited at  2026-02-08 00:18:07

--- FlaskDemo/app.py
+++ FlaskDemo/app.py
@@ -1,5 +1,20 @@
 from flask import Flask, render_template,request
+from dotenv import load_dotenv
+import os
+import pymongo
+
+load_dotenv()
+
+mongo_uri = os.getenv("MONGO_URI")
+    # Create a new client and connect to the DB server
+client = pymongo.MongoClient(mongo_uri)
+
 app = Flask(__name__)
+
+db= client.FLASK_DB
+
+collection = db['Flask_Data']
+
 
 @app.route('/')
 def home():
@@ -20,7 +35,17 @@ def submit():
     result = "Hi ! " + first_name + " " + last_name + ". \n Your Age is : " + age + " \n Your email is :" + email + "\n Your password is : "  + pwd + " \n\n\n  Your form has been submitted successfully."
     
     form_data = dict(request.form)
-    return form_data
+    collection.insert_one(form_data)
+    return "Form submitted successfully! Thank you, " + first_name + " " + last_name + "."
+
+@app.route('/database_view')
+def data():
+    data = collection.find()
+    output = list(data)
+
+    for item in output:
+        del item['_id']
+    return {"data": output}
 
 
 if __name__ == '__main__':
